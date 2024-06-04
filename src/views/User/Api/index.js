941a08fcfb92ba5ad23490b19a7d348dbf14a9da
unknown <mikiyasdaniel983@gmail.com>
2024-06-04 16:42:51 +0300

    doneo

---
+++ src/views/User/Api/index.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import buildPaginationQueryOpts from '../../../utils/sorts';
+
+export default class UserManagementService {
+    get(userId) {
+        return axios.get(`api/admin/users/${userId}`);
+    }
+
+    create(user) {
+        return axios.post('api/admin/users', user);
+    }
+
+    update(user) {
+        return axios.put('api/admin/users', user);
+    }
+
+    remove(userId) {
+        return axios.delete(`api/admin/users/${userId}`);
+    }
+
+    retrieve(req) {
+        return axios.get(`api/admin/users?${buildPaginationQueryOpts(req)}`);
+    }
+
+    retrieveAuthorities() {
+        return axios.get('api/authorities');
+    }
+}
